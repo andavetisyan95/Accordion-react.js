323392f519d2a64aa2ba78e3e03b9dd38398ada7
Ando Avetisyan <andoavetis0127@gmail.com>
2022-05-02 14:24:32 +0400

    content.js: created for accordion's title and body

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Accordion from './components/Accordion';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Accordion />
     </div>
   );
 }
---
+++ src/sources/content.js
@@ -0,0 +1,21 @@
+export const content = [
+    {
+        title: "Section 1",
+        body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+
+    },
+
+    {
+        title: "Section 2",
+        body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+
+    {
+        title: "Section 3",
+        body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    
+    {   title: "Section 4",
+        body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    }
+]
