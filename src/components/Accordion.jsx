70b766765c03f89d98e7d11ec93caf506e11e2b7
Ando Avetisyan <andoavetis0127@gmail.com>
2022-05-02 16:32:56 +0400

    Accordion: text open and close logic

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,22 +1,37 @@
+//react hooks
 import React, { useState } from 'react';
-import { content } from "../sources/content"
+//helpers
+import { content } from "../sources/content";
+//styles
+import styles from "../styles/Accordion.module.scss"
 
 
 export default function Accordion() {
 
-    // const [showText, setShowText] = useState(false);
+    const [showContent, setShowContent] = useState(false);
+
+    const showText = (i)=>{
+       return showContent === i ? setShowContent(false) : setShowContent(i)
+    }
 
     return (
-        <div>
+            <div className={styles.container}>
             {
-                content.map(({title})=>{
-                    return(
-                        <div className={styles.content_div}> 
-
+                content.map(({title,body},i)=>
+                    (
+                        <div  key={`${i}_${title}`} className={styles.container_div}> 
+                            <div className={styles.container_div_title}>
+                                <h4>{title}</h4>
+                                <span onClick={()=>showText(i)}>{showContent === i ? "-" : "+"}</span>
+                            </div>
+                            <div className={`${showContent === i ? styles.showText : styles.container_div_text } `}>
+                                {body}
+                            </div>
                         </div>
+                        
                     )
-                })
+                )
             }
-        </div>
+            </div>
     )
 }
