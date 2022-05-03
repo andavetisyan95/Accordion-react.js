15dea973159105aa5e9e15ec06166443e08fa8b5
Ando Avetisyan <andoavetis0127@gmail.com>
2022-05-03 21:03:07 +0400

    Accordion: added animation with transition

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,37 +1,50 @@
 //react hooks
-import React, { useState } from 'react';
+import React, { useState } from "react";
 //helpers
 import { content } from "../sources/content";
 //styles
-import styles from "../styles/Accordion.module.scss"
-
+import styles from "../styles/Accordion.module.scss";
 
 export default function Accordion() {
+  const [index, setIndex] = useState([]);
 
-    const [showContent, setShowContent] = useState(false);
-
-    const showText = (i)=>{
-       return showContent === i ? setShowContent(false) : setShowContent(i)
+  const showText = (i) => {
+    if (!index.includes(i)) {
+      if (index.length < 3) {
+        setIndex(index.concat(i));
+      } else {
+        index.shift();
+      }
+    } else {
+      setIndex(index.filter((el) => el !== i));
     }
-
-    return (
-            <div className={styles.container}>
-            {
-                content.map(({title,body},i)=>
-                    (
-                        <div  key={`${i}_${title}`} className={styles.container_div}> 
-                            <div className={styles.container_div_title}>
-                                <h4>{title}</h4>
-                                <span onClick={()=>showText(i)}>{showContent === i ? "-" : "+"}</span>
-                            </div>
-                            <div className={`${showContent === i ? styles.showText : styles.container_div_text } `}>
-                                {body}
-                            </div>
-                        </div>
-                        
-                    )
-                )
-            }
-            </div>
-    )
+  };
+  console.log(index);
+  console.log(index.length);
+  return (
+    <div className={styles.container}>
+      {content.map(({ title, body, id }, i) => (
+        <div key={`${id}_${title}`} className={styles.container_div}>
+          <div className={styles.container_div_title}>
+            <h4 className={`${index.includes(i) ? styles.changeColor : null}`}>
+              {title}
+            </h4>
+            <span
+              className={`${index.includes(i) ? styles.roatet : null}`}
+              onClick={() => showText(i)}
+            >
+              {index.includes(i) ? "-" : "+"}
+            </span>
+          </div>
+          <div
+            className={`${
+              index.includes(i) ? styles.showText : styles.container_div_text
+            } `}
+          >
+            {body}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
--- src/sources/content.js
+++ src/sources/content.js
@@ -1,26 +1,25 @@
 export const content = [
-    {
-        id: 1,
-        title: "What is Lorem Ipsum?",
-        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  {
+    id: 1,
+    title: "What is Lorem Ipsum?",
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
 
-    },
+  {
+    id: 2,
+    title: "Where does it come from?",
+    body: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+  },
 
-    {
-        id: 2,
-        title: "Where does it come from?",
-        body: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."
-    },
+  {
+    id: 3,
+    title: "Why do we use it?",
+    body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.",
+  },
 
-    {
-        id: 3,
-        title: "Why do we use it?",
-        body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters."
-    },
-    
-    {   
-        id: 4,
-        title: "Where can I get some?",
-        body: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
-    }
-]
+  {
+    id: 4,
+    title: "Where can I get some?",
+    body: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+  },
+];
